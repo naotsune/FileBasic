49e5680be32ebd3e6ca71fde052f62c3d6655f10
Naoki Tsunekawa <tsunekawa@alde.co.jp>
2017-11-15 08:22:47 +0900

    aaa

---
+++ app/src/main/java/com/practice/naotsune/filebasic/MainActivity.java
@@ -0,0 +1,46 @@
+package com.practice.naotsune.filebasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import android.widget.EditText;
+import android.view.View;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtMemo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //EditTextを取得
+        txtMemo = (EditText)findViewById(R.id.txtMemo);
+    }
+
+    public void onClick(View view) {
+        int aaa = 5;
+        try {
+            //memo.datへの書き込みを準備
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            openFileOutput("memo.dat", Context.MODE_PRIVATE)));
+            //EditTextへの入力値をファイルに書き込み
+            String str = txtMemo.getText().toString();
+            writer.write(str);
+            //ファイルをクローズ
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
